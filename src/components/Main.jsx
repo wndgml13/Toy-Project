0cdadaeef955fedfdf2602a3626ff0c234807aac
wndgml13 <wndgml13>
2024-01-07 18:50:37 +0900

    첫 번째 커밋

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import TodoList from "./TodoList";
+
+export default function Main() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default function TodoList() {
+  return (
+    <>
+      <Header>To-Do List</Header>
+      <hr />
+    </>
+  );
+}
+
+const Header = styled.h2`
+  padding: 15px;
+`;
